4c9cf8d2ce0b2fceb13c3f338f23f6c2ef707391
xiethon <xiethon@126.com>
2021-02-27 16:58:01 +0800

    import ffmpeg

--- MoonPlayer/main.cpp
+++ MoonPlayer/main.cpp
@@ -1,6 +1,13 @@
-
+extern "C" {
+	#include "libavformat/avformat.h"
+	#include "libavcodec/avcodec.h"
+	#include "libswscale/swscale.h"
+	#include "libswresample/swresample.h"
+}
+#include <stdio.h>
 
 int main()
 {
+	av_register_all();//³õÊ¼»¯·â×°¿â
 	return 0;
 }
